9da52e200315ccef44bb2fa293c25a3bc3937cf5
Evgeniy Boyko <jekaboyko0605@gmail.com>
2024-03-05 04:02:18 +0200

    fix header

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,8 +5,6 @@ const refs = {
   modalForm: document.querySelector('[data-modal-form]'),
 };
 
-console.log(refs.openModalBtns);
-
 refs.openModalBtns.forEach(btn => {
   btn.addEventListener('click', onOpenModal);
 });
@@ -14,33 +12,16 @@ refs.closeModalBtn.addEventListener('click', onCloseModal);
 refs.backdrop.addEventListener('click', onBackdropClick);
 
 function onOpenModal() {
-  disableScroll();
-
   window.addEventListener('keydown', onEscKeyPress);
   refs.backdrop.classList.remove('is-hidden');
   // document.body.classList.add('modal-is-open');
 }
 
 function onCloseModal() {
-  enableScroll();
-
   window.removeEventListener('keydown', onEscKeyPress);
   refs.backdrop.classList.add('is-hidden');
   // document.body.classList.remove('modal-is-open');
-
-  const body = document.body;
-  const scrollY = body.style.top;
-  body.style.position = '';
-  body.style.top = '';
-  window.scrollTo(0, parseInt(scrollY || '0') * -1);
 }
-
-window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty(
-    '--scroll-y',
-    `${window.scrollY}px`
-  );
-});
 
 function onBackdropClick(evt) {
   if (evt.currentTarget === evt.target) {
@@ -55,21 +36,6 @@ function onEscKeyPress(evt) {
   if (isEscKey) {
     onCloseModal();
   }
-}
-
-function disableScroll() {
-  // Get the current page scroll position
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-  // if any scroll is attempted,
-  // set this to the previous value
-  window.onscroll = function () {
-    window.scrollTo(scrollLeft, scrollTop);
-  };
-}
-
-function enableScroll() {
-  window.onscroll = function () {};
 }
 
 // Data placeholders
